03e37ba5567c3558eef3231173dac8b71c8edf0f
CupRacer <CupRacer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-20 23:10:15 +0000

    some corrections and improvements; see readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/fadeout-thumbshots/tags/1.6@195980 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fadeout.php
+++ fadeout.php
@@ -4,7 +4,7 @@ Plugin Name: FadeOut-Thumbshots
 Plugin URI: http://www.mynakedgirlfriend.de/wordpress/fadeout-thumbshots/
 Description: 
 Author: Thomas Schulte
-Version: 1.2
+Version: 1.6
 Author URI: http://www.mynakedgirlfriend.de
 
 Copyright (C) 2010 Thomas Schulte
@@ -24,9 +24,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+
+
 $version = get_option('ts_fadeout_version');
-if($version == '') {
-	add_option('ts_fadeout_version','1.2','Version of the plugin FadeOut-Thumbshots','yes');
+if($version == '' || $version != "1.6") {
+	add_option('ts_fadeout_version','1.6','Version of the plugin FadeOut-Thumbshots','yes');
 }
 
 $active = get_option('ts_fadeout_active');
@@ -43,6 +45,18 @@ $showfooter = get_option('ts_fadeout_showfooter');
 if($showfooter == '') {
 	add_option('ts_fadeout_showfooter','yes');
 }
+
+$scaling = get_option('ts_fadeout_scaling');
+if($scaling == '') {
+	add_option('ts_fadeout_scaling','5');
+}
+
+$opacity = get_option('ts_fadeout_opacity'); 
+if($opacity == '') {
+	add_option('ts_fadeout_opacity','90');
+}
+
+
 
 /* actions */
 add_action( 'admin_menu', 'ts_fadeout_options_page' ); // add option page
@@ -74,6 +88,8 @@ function ts_fadeout_options(){
 		$active = $_POST['active'];
 		$preview = $_POST['preview'];
     		$showfooter = $_POST['showfooter'];
+		$scaling = $_POST['scaling'];
+		$opacity = $_POST['opacity'];
 
 		if($active == 'yes') {
 			update_option('ts_fadeout_active','yes');
@@ -95,12 +111,22 @@ function ts_fadeout_options(){
 			update_option('ts_fadeout_showfooter','no');
 		}
 
+		if(in_array($scaling, array("2", "3", "4", "5", "6", "7", "8"))) {
+			update_option('ts_fadeout_scaling',$scaling);
+		}
+
+		if(is_numeric($opacity)) {
+			update_option('ts_fadeout_opacity',$opacity);
+		}
+
 		echo('<div id="message" class="updated fade"><p><strong>Your options were saved.</strong></p></div>');
 	}
 
 	$active = get_option('ts_fadeout_active');
 	$preview = get_option('ts_fadeout_preview');
 	$showfooter = get_option('ts_fadeout_showfooter');
+	$scaling = get_option('ts_fadeout_scaling');
+	$opacity = get_option('ts_fadeout_opacity');
   
 	echo('<div class="wrap">');
 	echo('<form method="post" accept-charset="utf-8">');
@@ -110,7 +136,8 @@ function ts_fadeout_options(){
 	echo('<li>Set the option "Plugin active" to "no" if you don\'t want to show tooltips, this way you shouldn\'t deactivate the plugin in case you don\'t want to show the tooltips for a while.</li>');
 	echo('<li>Tooltips can be used for three types of links. "All" just means all links that exist on a page and "external" hides the thumbshots for internal links.</li>');
 	echo('<li>Using the option value "marked" means, that the tooltip-thumbshots are only shown if a link has a style class named "fadeout".</li>');
-	echo('<li>Although it\'s up to you to decide whether you\'d like to place a backlink on your site or not, the Fadeout homepage says that using their thumbshots requires a backlink to their site.</li>');
+	echo('<li>The opacity may be set according your needs. I prefer using "0.1", "0.2"... "1" to adjust the opacity.</li>');
+	echo('<li>Although it\'s up to you to decide whether you\'d like to place a backlink on your site or not, the Fadeout homepage says that using their thumbshots requires a backlink to their site. You can enable/disable the footer info with the corresponding select field. The footer was developed very roughly, so if you like the plugin, please link the two domains <a href="http://www.fadeout.de">www.fadeout.de</a> and <a href="http://www.mynakedgirlfriend.de">www.mynakedgirlfriend.de</a> somewhere in your blog. Thanks!</li>');
 	echo('</ol>');
 	echo('<br>');
 	echo('
@@ -136,12 +163,32 @@ function ts_fadeout_options(){
 				</td>
 			</tr>
 			<tr>
+				<td>tooltip preview size:&nbsp;</td>
+				<td>
+					<select name="scaling" id="scaling">
+						<option value="2"'); if ($scaling == '2') echo(' selected=selected'); echo('>463 x 523</option>
+						<option value="3"'); if ($scaling == '3') echo(' selected=selected'); echo('>343 x 373</option>
+						<option value="4"'); if ($scaling == '4') echo(' selected=selected'); echo('>278 x 298</option>
+						<option value="5"'); if ($scaling == '5') echo(' selected=selected'); echo('>241 x 253</option>
+						<option value="6"'); if ($scaling == '6') echo(' selected=selected'); echo('>216 x 223</option>
+						<option value="7"'); if ($scaling == '7') echo(' selected=selected'); echo('>198 x 201</option>
+						<option value="8"'); if ($scaling == '8') echo(' selected=selected'); echo('>185 x 185</option>
+					</select> Pixel
+				</td>
+			</tr>
+			<tr>
 				<td>show plugin info in footer:&nbsp;</td>
 				<td>
 					<select name="showfooter" id="showfooter">
 						<option value="yes" label="yes"'); if ($showfooter == 'yes') echo(' selected=selected'); echo('>yes</option>
 						<option value="no" label="no"'); if ($showfooter == 'no') echo(' selected=selected'); echo('>no</option>
 					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>tooltip opacity:&nbsp;</td>
+				<td>
+					<input type="text" size="3" maxlength="3" name="opacity" value="' . $opacity . '">
 				</td>
 			</tr>
 		</table>');  
@@ -159,7 +206,72 @@ function ts_fadeout_options(){
 
 function ts_fadeout_header() {
 	$header.= '<link rel="stylesheet" href="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/jquery-tooltip/jquery.tooltip.css" />' . "\n";
-	$header.= '<link rel="stylesheet" href="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/css/style.css" />' . "\n";
+
+	$header.= '<style type="text/css">
+		<!--
+			#tooltip.pretty {
+				font-family: Arial;
+				border: none;';
+
+				switch(get_option('ts_fadeout_scaling')) {
+					case 2:
+						$header.= 'width: 463px; height: 523px;';
+						break;
+					case 3:
+						$header.= 'width: 343px; height: 373px;';
+						break;
+					case 4:
+						$header.= 'width: 278px; height: 298px;';
+						break;
+					case 5:
+						$header.= 'width: 241x; height: 253px;';
+						break;
+					case 6:
+						$header.= 'width: 216px; height: 223px;';
+						break;
+					case 7:
+						$header.= 'width: 198px; height: 201px;';
+						break;
+					case 8:
+						$header.= 'width: 185px; height: 185px;';
+						break;
+				}
+
+	$header.= '		padding:0px;
+				opacity: ' . get_option("ts_fadeout_opacity") . ';
+				background: url("' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/shadow-' . get_option('ts_fadeout_scaling') . '.png");
+			}
+
+			#tooltip.pretty div {
+				text-align: left;';
+
+				switch(get_option('ts_fadeout_scaling')) {
+					case 2:
+						$header.= 'width: 463px; padding-left: 38px; padding-top:32px;';
+						break;
+					case 3:
+						$header.= 'width: 343px; padding-left:38px; padding-top:32px;';
+						break;
+					case 4:
+						$header.= 'width: 278px; padding-left:38px; padding-top:32px;';
+						break;
+					case 5:
+						$header.= 'width: 200px; padding-left:38px; padding-top:32px;';
+						break;
+					case 6:
+						$header.= 'width: 216px; padding-left:38px; padding-top:32px;';
+						break;
+					case 7:
+						$header.= 'width: 198px; padding-left:38px; padding-top:32px;';
+						break;
+					case 8:
+						$header.= 'width: 185px; padding-left:38px; padding-top:32px;';
+						break;
+				}
+
+	$header.= '}
+		-->
+		</style>';
 
 	$header.= '<script type="text/javascript" src="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/jquery-tooltip/lib/jquery.js"></script>' . "\n";
 	$header.= '<script type="text/javascript" src="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/jquery-tooltip/lib/jquery.bgiframe.js"></script>' . "\n";
@@ -205,7 +317,7 @@ function ts_fadeout_header() {
 						left: 5,
 						fade: 250,
 						bodyHandler: function() {
-							image = "http://fadeout.de/thumbshot-pro/?scale=5&url=" + this + "&wp=1";
+							image = "http://fadeout.de/thumbshot-pro/?scale=' . get_option('ts_fadeout_scaling') . '&url=" + this + "&wp=1";
 							return $("<img />").attr("src", image);';
 
 						$header.= '}
@@ -219,9 +331,8 @@ function ts_fadeout_header() {
 
 
 function ts_fadeout_footer() {
-	$footer.= '<div style="text-align:center;"><a href="http://fadeout.de/"><img style="vertical-align:middle;" src="http://fadeout.de/images/banner-80x15.gif"></a>&nbsp;Plugin by <a href="http://www.mynakedgirlfriend.de">MyNakedGirlfriend.de</a></div>';
+	$footer.= '<div style="text-align:center;"><a href="http://fadeout.de/"><img style="vertical-align:middle;" src="http://fadeout.de/images/banner-80x15.gif" alt="FadeOut-Thumbshots"></a>&nbsp;Plugin by <a href="http://www.mynakedgirlfriend.de">MyNakedGirlfriend.de</a></div>';
 	print($footer);
 }
 
 ?>
-
