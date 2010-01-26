35b48b3940e39ce593ae095968f43c70f230fde8
CupRacer <CupRacer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-26 18:39:38 +0000

    some corrections and improvements; see readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/fadeout-thumbshots/tags/1.999@198415 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fadeout.php
+++ fadeout.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: FadeOut-Thumbshots
 Plugin URI: http://www.mynakedgirlfriend.de/wordpress/fadeout-thumbshots/
-Description: 
+Description: This plugin dynamically shows a preview tooltip for hyperlinks on your WordPress site.
 Author: Thomas Schulte
-Version: 1.2
+Version: 1.999
 Author URI: http://www.mynakedgirlfriend.de
 
 Copyright (C) 2010 Thomas Schulte
@@ -24,25 +24,49 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$version = get_option('ts_fadeout_version');
-if($version == '') {
-	add_option('ts_fadeout_version','1.2','Version of the plugin FadeOut-Thumbshots','yes');
+
+
+$ts_fadeout_version = get_option('ts_fadeout_version');
+if($ts_fadeout_version == '' || $ts_fadeout_version != "1.999") {
+	add_option('ts_fadeout_version','1.999','Version of the plugin FadeOut-Thumbshots','yes');
 }
 
-$active = get_option('ts_fadeout_active');
-if($active == '') {
+$ts_fadeout_active = get_option('ts_fadeout_active');
+if($ts_fadeout_active == '') {
 	add_option('ts_fadeout_active','yes');
 }
 
-$preview = get_option('ts_fadeout_preview');
-if($preview == '') {
+$ts_fadeout_preview = get_option('ts_fadeout_preview');
+if($ts_fadeout_preview == '') {
 	add_option('ts_fadeout_preview','all');
 }
 
-$showfooter = get_option('ts_fadeout_showfooter');
-if($showfooter == '') {
+$ts_fadeout_showfooter = get_option('ts_fadeout_showfooter');
+if($ts_fadeout_showfooter == '') {
 	add_option('ts_fadeout_showfooter','yes');
 }
+
+$ts_fadeout_scaling = get_option('ts_fadeout_scaling');
+if($ts_fadeout_scaling == '') {
+	add_option('ts_fadeout_scaling','5');
+}
+
+$ts_fadeout_opacity = get_option('ts_fadeout_opacity'); 
+if($ts_fadeout_opacity == '') {
+	add_option('ts_fadeout_opacity','0.9');
+}
+
+$ts_fadeout_dummylang = get_option('ts_fadeout_dummylang');
+if($ts_fadeout_dummylang == '') {
+	add_option('ts_fadeout_dummylang','de');
+}
+
+$ts_fadeout_code_placement = get_option('ts_fadeout_code_placement');
+if($ts_fadeout_code_placement == '') {
+	add_option('ts_fadeout_code_placement','head');
+}
+
+
 
 /* actions */
 add_action( 'admin_menu', 'ts_fadeout_options_page' ); // add option page
@@ -53,9 +77,16 @@ if(get_option('ts_fadeout_active') == 'yes') {
 
 
 function ts_fadeout_add_plugin() {
-	add_action('wp_head', 'ts_fadeout_header');
+	add_action('wp_head', 'ts_fadeout_styles');
+
+	$code_placement = get_option('ts_fadeout_code_placement');
+	if($code_placement == "head") {
+		add_action('wp_head', 'ts_fadeout_scripts');
+	}else {
+		add_action('wp_footer', 'ts_fadeout_scripts');
+	}
 	if(get_option('ts_fadeout_showfooter') == 'yes') {
-		add_action('wp_footer', 'ts_fadeout_footer');
+		add_action('wp_footer', 'ts_fadeout_about');
 	}
 }
 
@@ -74,6 +105,11 @@ function ts_fadeout_options(){
 		$active = $_POST['active'];
 		$preview = $_POST['preview'];
     		$showfooter = $_POST['showfooter'];
+		$scaling = $_POST['scaling'];
+		$opacity = $_POST['opacity'];
+		$dummylang = $_POST['dummylang'];
+		$code_placement = $_POST['code_placement'];
+		$include_pages = $_POST['include_pages'];
 
 		if($active == 'yes') {
 			update_option('ts_fadeout_active','yes');
@@ -95,12 +131,36 @@ function ts_fadeout_options(){
 			update_option('ts_fadeout_showfooter','no');
 		}
 
+		if(in_array($scaling, array("2", "3", "4", "5", "6", "7", "8"))) {
+			update_option('ts_fadeout_scaling',$scaling);
+		}
+
+		if(is_numeric($opacity)) {
+			update_option('ts_fadeout_opacity',$opacity);
+		}
+
+		if(in_array($dummylang, array("en", "de"))) {
+			update_option('ts_fadeout_dummylang',$dummylang);
+		}
+
+		if(in_array($code_placement, array("head", "footer"))) {
+			update_option('ts_fadeout_code_placement',$code_placement);
+		}
+
+		//FIXME: validation missing; overwrite even if empty
+		update_option('ts_fadeout_include_pages',$include_pages);
+
 		echo('<div id="message" class="updated fade"><p><strong>Your options were saved.</strong></p></div>');
 	}
 
 	$active = get_option('ts_fadeout_active');
 	$preview = get_option('ts_fadeout_preview');
 	$showfooter = get_option('ts_fadeout_showfooter');
+	$scaling = get_option('ts_fadeout_scaling');
+	$opacity = get_option('ts_fadeout_opacity');
+	$dummylang = get_option('ts_fadeout_dummylang');
+	$code_placement = get_option('ts_fadeout_code_placement');
+	$include_pages = get_option('ts_fadeout_include_pages');
   
 	echo('<div class="wrap">');
 	echo('<form method="post" accept-charset="utf-8">');
@@ -110,7 +170,10 @@ function ts_fadeout_options(){
 	echo('<li>Set the option "Plugin active" to "no" if you don\'t want to show tooltips, this way you shouldn\'t deactivate the plugin in case you don\'t want to show the tooltips for a while.</li>');
 	echo('<li>Tooltips can be used for three types of links. "All" just means all links that exist on a page and "external" hides the thumbshots for internal links.</li>');
 	echo('<li>Using the option value "marked" means, that the tooltip-thumbshots are only shown if a link has a style class named "fadeout".</li>');
-	echo('<li>Although it\'s up to you to decide whether you\'d like to place a backlink on your site or not, the Fadeout homepage says that using their thumbshots requires a backlink to their site.</li>');
+	echo('<li>The opacity may be set according your needs. I prefer using "0.1", "0.2"... "1" to adjust the opacity.</li>');
+	echo('<li>Although it\'s up to you to decide whether you\'d like to place a backlink on your site or not, the Fadeout homepage says that using their thumbshots requires a backlink to their site. You can enable/disable the footer info with the corresponding select field. The footer was developed very roughly, so if you like the plugin, please link the two domains <a href="http://www.fadeout.de">www.fadeout.de</a> and <a href="http://www.mynakedgirlfriend.de">www.mynakedgirlfriend.de</a> somewhere in your blog. Thanks!</li>');
+	echo('<li>The option "code placement" defines the place where the plugin javascript is integrated. The default is "head" (which means the html head tag) but it\'s also possible to put the code at the end of your page by choosing "footer"</li>');
+	echo('<li>While in "external" or "all" mode, you can limit the plugin to explicit defined pages with the option "include pages". Just leave it empty to ignore this feature. :-)</li>');
 	echo('</ol>');
 	echo('<br>');
 	echo('
@@ -136,12 +199,56 @@ function ts_fadeout_options(){
 				</td>
 			</tr>
 			<tr>
+				<td>tooltip preview size:&nbsp;</td>
+				<td>
+					<select name="scaling" id="scaling">
+						<option value="2"'); if ($scaling == '2') echo(' selected=selected'); echo('>463 x 523</option>
+						<option value="3"'); if ($scaling == '3') echo(' selected=selected'); echo('>343 x 373</option>
+						<option value="4"'); if ($scaling == '4') echo(' selected=selected'); echo('>278 x 298</option>
+						<option value="5"'); if ($scaling == '5') echo(' selected=selected'); echo('>241 x 253</option>
+						<option value="6"'); if ($scaling == '6') echo(' selected=selected'); echo('>216 x 223</option>
+						<option value="7"'); if ($scaling == '7') echo(' selected=selected'); echo('>198 x 201</option>
+						<option value="8"'); if ($scaling == '8') echo(' selected=selected'); echo('>185 x 185</option>
+					</select> Pixel
+				</td>
+			</tr>
+			<tr>
 				<td>show plugin info in footer:&nbsp;</td>
 				<td>
 					<select name="showfooter" id="showfooter">
 						<option value="yes" label="yes"'); if ($showfooter == 'yes') echo(' selected=selected'); echo('>yes</option>
 						<option value="no" label="no"'); if ($showfooter == 'no') echo(' selected=selected'); echo('>no</option>
 					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>tooltip opacity:&nbsp;</td>
+				<td>
+					<input type="text" size="3" maxlength="3" name="opacity" value="' . $opacity . '">
+				</td>
+			</tr>
+			<tr>
+				<td>dummy lang:&nbsp;</td>
+				<td>
+					<select name="dummylang" id="dummylang">
+						<option value="en" label="en"'); if ($dummylang == 'en') echo(' selected=selected'); echo('>english</option>
+						<option value="de" label="de"'); if ($dummylang == 'de') echo(' selected=selected'); echo('>german</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>code placement:&nbsp;</td>
+				<td>
+					<select name="code_placement" id="code_placement">
+						<option value="head" label="head"'); if ($code_placement == 'head') echo(' selected=selected'); echo('>head</option>
+						<option value="footer" label="footer"'); if ($code_placement == 'footer') echo(' selected=selected'); echo('>footer</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>include page ID\'s:&nbsp;</td>
+				<td>
+					<input type="text" size="20" name="include_pages" value ="' . $include_pages . '"> (separate with whitespaces - keep field empty to include all)
 				</td>
 			</tr>
 		</table>');  
@@ -157,9 +264,105 @@ function ts_fadeout_options(){
 }
 
 
-function ts_fadeout_header() {
+
+function ts_fadeout_get_include_pages() {
+	$keywords = NULL;
+	$include_pages = get_option('ts_fadeout_include_pages');
+	if($include_pages != "") {
+		$keywords = preg_split("/[\s,]+/", $include_pages);
+	}
+	return $keywords;
+}
+
+
+function ts_fadeout_styles() {
+	if((get_option('ts_fadeout_preview') == "all" || get_option('ts_fadeout_preview') == "external") && sizeof(ts_fadeout_get_include_pages()) > 0) {
+		global $post;
+		if(!in_array($post->ID, ts_fadeout_get_include_pages())) {
+			return;
+		}
+	}
+
 	$header.= '<link rel="stylesheet" href="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/jquery-tooltip/jquery.tooltip.css" />' . "\n";
-	$header.= '<link rel="stylesheet" href="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/css/style.css" />' . "\n";
+
+	$header.= '<style type="text/css">
+		<!--
+			#tooltip.pretty {
+				font-family: Arial;
+				border: none;';
+
+				switch(get_option('ts_fadeout_scaling')) {
+					case 2:
+						$header.= 'width: 463px; height: 523px;';
+						break;
+					case 3:
+						$header.= 'width: 343px; height: 373px;';
+						break;
+					case 4:
+						$header.= 'width: 278px; height: 298px;';
+						break;
+					case 5:
+						$header.= 'width: 241x; height: 253px;';
+						break;
+					case 6:
+						$header.= 'width: 216px; height: 223px;';
+						break;
+					case 7:
+						$header.= 'width: 198px; height: 201px;';
+						break;
+					case 8:
+						$header.= 'width: 185px; height: 185px;';
+						break;
+				}
+
+	$header.= '		padding:0px;
+				opacity: ' . get_option("ts_fadeout_opacity") . ';
+				background: url("' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/shadow-' . get_option('ts_fadeout_scaling') . '.png");
+			}
+
+			#tooltip.pretty div {
+				text-align: left;';
+
+				switch(get_option('ts_fadeout_scaling')) {
+					case 2:
+						$header.= 'width: 463px; padding-left: 38px; padding-top:32px;';
+						break;
+					case 3:
+						$header.= 'width: 343px; padding-left:38px; padding-top:32px;';
+						break;
+					case 4:
+						$header.= 'width: 278px; padding-left:38px; padding-top:32px;';
+						break;
+					case 5:
+						$header.= 'width: 200px; padding-left:38px; padding-top:32px;';
+						break;
+					case 6:
+						$header.= 'width: 216px; padding-left:38px; padding-top:32px;';
+						break;
+					case 7:
+						$header.= 'width: 198px; padding-left:38px; padding-top:32px;';
+						break;
+					case 8:
+						$header.= 'width: 185px; padding-left:38px; padding-top:32px;';
+						break;
+				}
+
+	$header.= '}
+		-->
+		</style>';
+
+	print $header;
+}
+
+
+
+function ts_fadeout_scripts() {
+	if((get_option('ts_fadeout_preview') == "all" || get_option('ts_fadeout_preview') == "external") && sizeof(ts_fadeout_get_include_pages()) > 0) {
+		global $post;
+		if(!in_array($post->ID, ts_fadeout_get_include_pages())) {
+			return;
+		}
+	}
 
 	$header.= '<script type="text/javascript" src="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/jquery-tooltip/lib/jquery.js"></script>' . "\n";
 	$header.= '<script type="text/javascript" src="' . get_option("siteurl") . '/wp-content/plugins/fadeout-thumbshots/jquery-tooltip/lib/jquery.bgiframe.js"></script>' . "\n";
@@ -171,31 +374,28 @@ function ts_fadeout_header() {
 			$(document).ready(function(){
 				$(function() {';
 
-					if(get_option('ts_fadeout_preview') == 'all') {
+					if(get_option('ts_fadeout_preview') == 'all' || get_option('ts_fadeout_preview') == 'external') {
 
-						$header.= '$("a").tooltip({';
-
-					}else if(get_option('ts_fadeout_preview') == 'external') {
-				
 						$header.= 'for (var i = 0; i < document.links.length; ++i) {
 							var link = document.links[i];
 							var blogurl = "' . get_option("siteurl") . '";
 							var linkurl = String(link.href).substring(0, blogurl.length);
-							if(blogurl != linkurl) {
-								link.className=link.className + " fadeout";
+							var linkproto = String(link.href).substring(0, 4);
+
+							if(linkproto == "http") {';
+
+							if(get_option('ts_fadeout_preview') == 'external') {
+								$header.= 'if(blogurl != linkurl) {
+									link.className=link.className + " fadeout";
+								}';
+							}else {
+								$header.= 'link.className=link.className + " fadeout";';
 							}
-						};';
-
-						$header.= '$(".fadeout").tooltip({';
-
-					}else if(get_option('ts_fadeout_preview') == 'marked') {
-
-						$header.= '$(".fadeout").tooltip({';
+						$header.= '}};';
 
 					}
 
-
-					$header.= '
+					$header.= '$(".fadeout").tooltip({
 						track: true,
 						delay: 40,
 						showURL: false,
@@ -205,8 +405,8 @@ function ts_fadeout_header() {
 						left: 5,
 						fade: 250,
 						bodyHandler: function() {
-							image = "http://fadeout.de/thumbshot-pro/?scale=5&url=" + this + "&wp=1";
-							return $("<img />").attr("src", image);';
+							image = "http://fadeout.de/thumbshot-pro/?scale=' . get_option('ts_fadeout_scaling') . '&url=" + this + "&wp=1&lang=' . get_option('ts_fadeout_dummylang') . '";
+							return $("<img style=\"border: none;\" />").attr("src", image);';
 
 						$header.= '}
 					});
@@ -218,10 +418,9 @@ function ts_fadeout_header() {
 }
 
 
-function ts_fadeout_footer() {
-	$footer.= '<div style="text-align:center;"><a href="http://fadeout.de/"><img style="vertical-align:middle;" src="http://fadeout.de/images/banner-80x15.gif"></a>&nbsp;Plugin by <a href="http://www.mynakedgirlfriend.de">MyNakedGirlfriend.de</a></div>';
+function ts_fadeout_about() {
+	$footer.= '<div style="text-align:center;"><a href="http://fadeout.de/"><img style="vertical-align:middle;" src="http://fadeout.de/images/link.gif" alt="FadeOut-Thumbshots"></a>&nbsp;Plugin by <a href="http://www.mynakedgirlfriend.de">MyNakedGirlfriend.de</a></div>';
 	print($footer);
 }
 
 ?>
-
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === FadeOut-Thumbshots ===
 Contributors: CupRacer
 Donate link: http://www.mynakedgirlfriend.de/
-Tags: image, plugin, thumbshot, preview, tooltip
+Tags: image, plugin, thumbshot, preview, tooltip, thumbnail
 Requires at least: 2.9
 Tested up to: 2.9.1
-Stable tag: 1.2
+Stable tag: 1.99
 
 This plugin dynamically shows a preview tooltip for hyperlinks on your WordPress site.
 
@@ -42,6 +42,32 @@ Nope. :-)
 
 
 == Changelog ==
+
+= 1.999 =
+* Added an option to define where the javascript code should be placed on a page (in html head tag or the footer).
+* Added an input field for page IDs to limit the plugin to only explicitely defined pages. This is a very basic version at the moment.
+
+= 1.99 =
+* Changed a variable name which caused trouble with the start page definition (Thx Frank)
+
+= 1.9 =
+* Added new config option for a localized dummy tooltip (de = "Thumbshot wird erstellt"; en = "coming soon").
+
+= 1.8 =
+* Set thumbnail border to "none" because of some buggy css stylesheets.
+* Added a filter, so that only http* links are previewed automatically when the setting "external" or "all" is set. (Thx Bernhard)
+
+= 1.7 =
+* Deleted a needless whitespace character that might have caused some problems with other plugins.
+* Created some better background images (the tooltip border images). I hope you'll like them.
+
+= 1.4 =
+* Changed opacity from 0.8 to 0.9 for a better image representation.
+* Added option to adjust the tooltip opacity.
+* Corrected a padding value (at scaling 180px × 212px).
+
+= 1.3 =
+* Added the possibility to choose from different tooltip sizes.
 
 = 1.2 =
 * Surrounded the wp_head() function with the plugins_loaded() method
